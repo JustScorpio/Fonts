858ad24bff362dac150f83ee6c42c720af9b7b66
Kosarev <skorpiello@gmail.com>
2022-04-23 14:54:53 +0500

    Finished

---
+++ Fonts/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Fonts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.comboBoxColor = new System.Windows.Forms.ComboBox();
+            this.comboBoxFonts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonConfirm = new System.Windows.Forms.Button();
+            this.textBoxSize = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Location = new System.Drawing.Point(12, 12);
+            this.textBoxText.Multiline = true;
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(566, 104);
+            this.textBoxText.TabIndex = 1;
+            // 
+            // comboBoxColor
+            // 
+            this.comboBoxColor.FormattingEnabled = true;
+            this.comboBoxColor.Items.AddRange(new object[] {
+            "Чёрный",
+            "Синий",
+            "Зелёный",
+            "Жёлтый",
+            "Красный"});
+            this.comboBoxColor.Location = new System.Drawing.Point(457, 131);
+            this.comboBoxColor.Name = "comboBoxColor";
+            this.comboBoxColor.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxColor.TabIndex = 2;
+            this.comboBoxColor.Text = "Чёрный";
+            // 
+            // comboBoxFonts
+            // 
+            this.comboBoxFonts.FormattingEnabled = true;
+            this.comboBoxFonts.Items.AddRange(new object[] {
+            "Sans Serif",
+            "Times New Roman",
+            "Calibri",
+            "Helvetica"});
+            this.comboBoxFonts.Location = new System.Drawing.Point(61, 131);
+            this.comboBoxFonts.Name = "comboBoxFonts";
+            this.comboBoxFonts.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxFonts.TabIndex = 3;
+            this.comboBoxFonts.Text = "Sans Serif";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Шрифт:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Цвет:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Размер:";
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.Location = new System.Drawing.Point(503, 171);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfirm.TabIndex = 8;
+            this.buttonConfirm.Text = "Применить";
+            this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.Click += new System.EventHandler(this.buttonConfirm_Click);
+            // 
+            // textBoxSize
+            // 
+            this.textBoxSize.Location = new System.Drawing.Point(260, 131);
+            this.textBoxSize.Name = "textBoxSize";
+            this.textBoxSize.Size = new System.Drawing.Size(43, 20);
+            this.textBoxSize.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 205);
+            this.Controls.Add(this.textBoxSize);
+            this.Controls.Add(this.buttonConfirm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxFonts);
+            this.Controls.Add(this.comboBoxColor);
+            this.Controls.Add(this.textBoxText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.ComboBox comboBoxColor;
+        private System.Windows.Forms.ComboBox comboBoxFonts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonConfirm;
+        private System.Windows.Forms.TextBox textBoxSize;
+    }
+}
+
---
+++ Fonts/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fonts
+{
+    public partial class Form1 : Form
+    {
+        ComboBox.ObjectCollection fontAllowedItems;
+        ComboBox.ObjectCollection colorAllowedItems;
+        int size;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            fontAllowedItems = comboBoxFonts.Items;
+            colorAllowedItems = comboBoxColor.Items;
+        }
+
+        private void buttonConfirm_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                size = int.Parse(textBoxSize.Text);
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Размер шрифта должен быть положительным целым числом",
+                    "Нет, не так",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+
+                textBoxSize.Text = string.Empty;
+                return;
+            }
+
+            textBoxText.Font = new Font(comboBoxFonts.Text, size);
+            
+            switch (comboBoxColor.Text)
+            {
+                case "Чёрный":
+                    textBoxText.ForeColor = Color.Black;
+                    break;
+                case "Синий":
+                    textBoxText.ForeColor = Color.Blue;
+                    break;
+                case "Жёлтый":
+                    textBoxText.ForeColor = Color.Yellow;
+                    break;
+                case "Красный":
+                    textBoxText.ForeColor = Color.Red;
+                    break;
+                case "Зелёный":
+                    textBoxText.ForeColor = Color.Green;
+                    break;
+            }
+        }
+
+    }
+}
